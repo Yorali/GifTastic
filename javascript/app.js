b75b23e79dfbec6468c954b2637f316ff86e8551
Yorali <hideayeni@gmail.com>
2017-06-15 12:05:06 -0500

    Buttons added to page dynamically

--- javascript/app.js
+++ javascript/app.js
@@ -6,23 +6,37 @@
 
 $(document).ready(function() {
 
-$("#guitar-button").on("click", function() {
+	var topics = ['Guitar','Drums','Piano','Crow','Wilting','Distortion','Vortex','Uncertain'];
+
+	fillButtonBox();
+
+	function fillButtonBox() {
+		var boxx = $('#buttonsbox');
+		for (var i = 0; i < topics.length; i++) {
+			boxx.prepend($("<button class='botan'>" + topics[i] + "</h1>"))
+		}
+	};
+
+	$("#guitar-button").on("click", function() {
 
 
-	var apiKey = 'dc6zaTOxFJmzC'
-	var queryURL = "https://api.giphy.com/v1/gifs/search?q=guitar&api_key=dc6zaTOxFJmzC";
+		var apiKey = 'dc6zaTOxFJmzC';
+		var choice = $(this).text();
+		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + choice + "&api_key=" + apiKey;
 
 
 
-	$.ajax({
-		url: queryURL,
-		method: "GET"
-	}).done(function(response) {
-		var imageUrl = response.data[1].url;
-		console.log(imageUrl);
+		$.ajax({
+			url: queryURL,
+			method: "GET"
+		}).done(function(response) {
+			// create img tag in #gifsbox
+			// Prepend 
+			var imageUrl = response.data[1].url;
+			console.log(imageUrl);
+		});
+
 	});
-
-});
 
 });
 
