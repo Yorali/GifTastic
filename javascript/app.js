bbd15636c834721ceabe4112ab54d2b26b76d934
Yorali <hideayeni@gmail.com>
2017-06-15 12:27:43 -0500

    10 items chosen upon click functionality added

--- javascript/app.js
+++ javascript/app.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
 	fillButtonBox();
 
 	function fillButtonBox() {
+		// Fills buttonsbox with buttons classed 'ボタン'
 		var boxx = $('#buttonsbox');
 		for (var i = 0; i < topics.length; i++) {
 			boxx.append($("<button class='botan'>" + topics[i] + "</h1>"))
@@ -21,6 +22,7 @@ $(document).ready(function() {
 
 
 		var apiKey = 'dc6zaTOxFJmzC';
+		// Selects text of button chosen
 		var choice = $(this).text();
 		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + choice + "&api_key=" + apiKey;
 
@@ -32,6 +34,15 @@ $(document).ready(function() {
 		}).done(function(response) {
 			// create img tag in #gifsbox
 			// Prepend 
+			populate();
+
+			function populate() {
+				for (var i = 0; i < 10; i++) {
+					var materials = response.data[i].images.fixed_height_small.url;
+					console.log(materials);
+				}
+			}
+
 			var imageUrl = response.data[1].url;
 			console.log(imageUrl);
 		});
